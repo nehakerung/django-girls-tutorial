327a9cca7ec35d7b031c7cb28e80ee222c82b70c
nehakerung <nehakerung@gmail.com>
2024-09-24 10:40:35 +0100

    Added html for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+#template name and context not supposed to be ther just appeared
